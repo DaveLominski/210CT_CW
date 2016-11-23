1fc787405b77de1eed5bf10e069d86a285ef9837
Dave <dawidlominski996@gmail.com>
2016-11-23 14:43:19 +0000

    updated task 1

--- Week7_Q1.py
+++ Week7_Q1.py
@@ -1,63 +1,47 @@
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def isEmpty(self):
-        return self.items == []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def peek(self):
-        return self.items[len(self.items) - 1]
-
-    def size(self):
-        return len(self.items)
-
-class Vertex():
-
-    def __init__(self):
-        self.vertices = []
-
-
 class Graph():
 
     def __init__(self):
-        self.edges = []
-        self.v = Vertex()
+        self.nodes = []
+        self.edges = dict()
 
-    def addVertex(self, node):
-        self.v.vertices.append(node)
 
-    def addEdge(self, node1, node2):
-        if node1 in self.v.vertices and node2 in self.v.vertices:
-            edge = [node1, node2]
-            self.edges.append(edge)
-            return self.edges
+
+    def addVertex(self,value):
+        self.nodes.append(value)
+
+        if value in self.nodes:
+            self.edges[value] = []
         else:
             pass
+    def printVertices(self):
+        return self.nodes
 
+    def addEdges(self, node1, node2):
+        if node1 not in self.nodes:
+            self.nodes.append(node1)
 
-    def DFS(self, start):
-        S =
+        if node2 not in self.nodes:
+            self.nodes.append(node2)
 
-if __name__ == "__main__":
+        self.edges[node1].append(node2)
+        self.edges[node2].append(node1)
+
+if __name__ == '__main__':
     g = Graph()
-    v = Vertex()
     g.addVertex(1)
-    g.addVertex(2)
+    g.addVertex(5)
+    g.addVertex(3)
+    g.addVertex(4)
     g.addVertex(10)
-    g.addVertex(6)
-    g.addVertex(8)
-    g.addVertex(15)
-    g.addVertex(9)
-    g.addVertex(11)
-    g.addEdge(1,2)
-    g.addEdge(9,11)
-    g.addEdge(10,6)
-    g.addEdge(8, 15)
-    g.addEdge(6,2)
-    print(g.edges)
+    g.addVertex(7)
+    g.addEdges(1,5)
+    g.addEdges(1,3)
+    g.addEdges(10,7)
+    g.addEdges(4,10)
+    g.addEdges(5,10)
+    for node in g.edges:
+        print(node, ":", g.edges[node])
+    print(g.printVertices())
+
+
+
