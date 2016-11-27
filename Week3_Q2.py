0a7bf1c5975c9fc3b6c3172b85f7147f61bf06b5
Dave <dawidlominski996@gmail.com>
2016-11-27 18:18:33 +0000

    Updated prime number question

--- Week3_Q2.py
+++ Week3_Q2.py
@@ -1,16 +1,19 @@
-def primeCheck(n,d=0):
-    if n == 0:
-        return False
-    elif d == 0:
-        d = n
-        return primeCheck(n,d)
-    elif n == 1:
-        return False
-    elif n == d:
-        return True
-    elif n%d == 0:
-        return False
+def primeNumber(number, counter=None):
+    if counter == 1:
+        print(number, "is a prime number")
+    elif number < 2:
+        print(number, "is not a prime number")
+    elif counter == None:
+        counter = number-1
+        primeNumber(number, counter)
+    elif number % counter == 0:
+        print(number, "is not a prime number")
+    elif number % counter != 0:
+        primeNumber(number, counter - 1)
+
     else:
-        primeCheck(n,d+1)
-        
+        return primeNumber(number, counter - 1)
     
+
+
+primeNumber(29)
